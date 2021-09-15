1b3d82f1eaaef03a66cedeaba25dbaaf29733c2d
Joseph Rohde <joseph.jf.rohde@gmail.com>
2021-09-15 16:41:44 +0200

    generating shop model

---
+++ db/migrate/20210915144039_create_shops.rb
@@ -0,0 +1,10 @@
+class CreateShops < ActiveRecord::Migration[6.0]
+  def change
+    create_table :shops do |t|
+      t.string :name
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
