f3df319932e96a5f369f787bcc92907380a79137
Joseph Rohde <joseph.jf.rohde@gmail.com>
2021-12-02 12:37:08 +0100

    initializing map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,11 +24,15 @@ require("channels")
 
 // External imports
 import "bootstrap";
+// import "controllers";
 
-// Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+// Internal imports
+import { initMapbox } from '../plugins/init_mapbox';
+// import { initStarRating } from '../plugins/init_star_rating';
+// import { initAutocomplete } from '../plugins/init_autocomplete';
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
-});
+  initMapbox();
+  // initStarRating();
+  // initAutocomplete();
+})
---
+++ app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,38 @@
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+
+  if (mapElement) {
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/dittojoe/ckwovkach1hlj15r02ne5g0rv/',
+      center: [18.0600026, 59.3354662],
+      zoom: 11.15
+    });
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+      new mapboxgl.Marker({ "color": "#c44536" })
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
+        .addTo(map);
+      if (markers.length > 1) {
+        fitMapToMarkers(map, markers);
+      } else if (markers.length == 1) {
+        map.flyTo({center: [ marker.lng, marker.lat ], zoom: 14})
+      }
+    });
+  }
+};
+
+export { initMapbox };
+
