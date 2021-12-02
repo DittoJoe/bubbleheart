e96d9590b12ce7fd9136c79df2844551fe7c45c7
Joseph Rohde <joseph.jf.rohde@gmail.com>
2021-12-02 12:56:38 +0100

    map styling

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -21,7 +21,7 @@ const initMapbox = () => {
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window);
-      new mapboxgl.Marker({ "color": "#c44536" })
+      new mapboxgl.Marker({ "color": "#9989A9" })
         .setLngLat([ marker.lng, marker.lat ])
         .setPopup(popup)
         .addTo(map);
